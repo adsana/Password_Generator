5524344a2bc8af8c24a5285a1e76144f2a6c79e3
Adsana <adsanasayasith@gmail.com>
2020-05-09 00:32:32 -0400

    added character length prompt

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var length = prompt("Between 8 and 128 characters, how many characters would you like your password to be?")
+  if (length < 8 || length > 128){
+    alert("Length must be 8-128 characters")
+}
+
+  if (length >= 8 && length <= 128){
+  var uppercase = confirm("Would you like to use uppercase letters?")
+  var lowercase = confirm("Would you like to use lowercase letters?")
+  var numbers = confirm("would you like to use numbers?")
+  var symbols = confirm("would you like to use special characters?")
+  }
+  
+  if (uppercase != true && lowercase != true && numbers != true && symbols != true){
+    alert("You must select at least one character type!")
+}
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
